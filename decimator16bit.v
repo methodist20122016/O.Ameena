697ea90da4e506a9dfb3caba7e69bb3244658747
T Maharshi Sanand Yadav <63612136+methodist20122016@users.noreply.github.com>
2022-03-15 12:13:05 +0530

    Add files via upload

--- decimator16bit.v
+++ decimator16bit.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    11:47:52 03/15/2022 
+// Create Date:    12:06:25 03/15/2022 
 // Design Name: 
 // Module Name:    decimator16bit 
 // Project Name: 
@@ -18,10 +18,10 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module decimator16bit(CLK, VMOD, MODE, Q);
+module decimator16bit(CLK, VMOD, MODE, Q, timer, count);
 input CLK, VMOD, MODE;
-output [15:0] Q;
-reg[15:0] count, timer, Q=1'b0;
+output reg [15:0]Q=1'b0;
+output reg[15:0] count=1'b0, timer=1'b0;
 always @(posedge CLK)
 begin
 timer = timer + 1'b1;
@@ -34,7 +34,9 @@ always @(negedge CLK)
 begin
 if (timer == 16'hffff && MODE == 1'b0)
 count = 16'h0000;
-else if(timer == 16'h8000 && MODE == 1'b1)count = 16'h0000;
-else if(VMOD)count = count + 1'b1;
+else if(timer == 16'h8000 && MODE == 1'b1)
+count = 16'h0000;
+else if(VMOD)
+count = count + 1'b1;
 end
 endmodule
--- decimator16bit_TB.v
+++ decimator16bit_TB.v
@@ -4,7 +4,7 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   11:49:43 03/15/2022
+// Create Date:   12:06:51 03/15/2022
 // Design Name:   decimator16bit
 // Module Name:   F:/FPGA/decimator16bit/decimator16bit_TB.v
 // Project Name:  decimator16bit
@@ -31,24 +31,30 @@ module decimator16bit_TB;
 
 	// Outputs
 	wire [15:0] Q;
+	wire [15:0] timer;
+	wire [15:0] count;
 
 	// Instantiate the Unit Under Test (UUT)
 	decimator16bit uut (
 		.CLK(CLK), 
 		.VMOD(VMOD), 
 		.MODE(MODE), 
-		.Q(Q)
+		.Q(Q), 
+		.timer(timer), 
+		.count(count)
 	);
 
 	initial 
 	begin
-	$monitor($time,"CLK=%b, VMOD=%b, MODE=%b, Q=%b",CLK, VMOD, MODE, Q);
+	$monitor($time,"CLK=%b, VMOD=%b, MODE=%b, Q=%b, timer=%b, count=%b",CLK, VMOD, MODE, Q, timer, count);
 		// Initialize Inputs
 		CLK = 0;
 		VMOD = 0;
 		MODE = 0;
 	end
-      
-	always #5 CLK=~CLK;	
+   
+always #5 CLK=~CLK;   
+always #15 VMOD=VMOD+1;
+always #30 MODE=MODE+1;
 endmodule
 
