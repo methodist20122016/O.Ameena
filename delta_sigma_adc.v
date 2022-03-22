cac326c820f0e31efbb567fc7ccbc38cd8dc93a9
T Maharshi Sanand Yadav <63612136+methodist20122016@users.noreply.github.com>
2022-03-22 10:22:46 +0530

    Add files via upload

---
+++ delta_sigma_adc.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:18:05 03/22/2022 
+// Design Name: 
+// Module Name:    delta_sigma_adc 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module delta_sigma_adc
+#(
+    parameter W = 16//????
+)
+(
+    input   wire                clk     ,
+    input   wire                rst_n   ,
+
+    input   wire signed [W-1:0] din     ,//signed analog signal
+    output  reg                 dout     //PDM signal
+);
+wire signed [W-1:0]adc1b_max = {1'b0,{(W-1){1'b1}}};
+wire signed [W-1:0]adc1b_min = {1'b1,{(W-1){1'b0}}};
+wire signed [W-1:0]adc1b_out = (dout == 1'b1)?adc1b_max:
+                               (dout == 1'b0)?adc1b_min:
+                               'bx;                    
+reg  signed   [W*2-1:0]inte0,inte1;
+wire signed   [W*2-1:0]diff0  =   din     -   adc1b_out;
+wire signed   [W*2-1:0]rd0    =   diff0   +   inte0;
+wire signed   [W*2-1:0]diff1  =   rd0     -   adc1b_out;
+wire signed   [W*2-1:0]rd1    =   diff1   +   inte1;
+wire          comp            =   (rd1 > 0)?1'b1:1'b0;
+always@(posedge clk or negedge rst_n)begin
+    if ( !rst_n ) begin
+        dout    <= 1'b0;
+        inte0   <=  'b0;
+        inte1   <=  'b0;
+    end else begin
+        dout    <= comp;
+        inte0   <= rd0;
+        inte1   <= rd1;
+    end
+end
+endmodule
+
+
---
+++ pdm_audio.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:19:07 03/22/2022 
+// Design Name: 
+// Module Name:    pdm_audio 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module pdm_audio
+(
+    input   wire                clk     ,// FREQ
+    input   wire                rst_n   ,
+    // connect to FIFO
+    input   wire                rdaccess,// The FIFO data is ready,FIFO not empty
+    input   wire                rdclk   ,// FREQ/32=48Khz
+    output  reg                 rden    ,
+    input   wire        [31:0]  rddat   ,// {L[31:16],R[15:0]},signed
+    // microphone
+    output  wire                pdm_r   ,    
+    output  wire                pdm_l       
+);
+reg  [1:0]bf0;
+wire rdaccess_b = bf0[1];
+always@(posedge rdclk or negedge rst_n)begin if(!rst_n)bf0<='b0;else bf0<={bf0,rdaccess};end
+always@(posedge rdclk or negedge rst_n)begin
+    if ( !rst_n ) begin
+        rden<=1'd0;
+    end else begin
+        if(rdaccess_b)
+            rden<=1'd1;
+    end
+end
+delta_sigma_adc #(.W ( 16 ))
+delta_sigma_adc_r (
+    .clk                     ( clk            ),
+    .rst_n                   ( rst_n          ),
+    .din                     ( rddat   [15:0] ),
+    .dout                    ( pdm_r          )
+);
+delta_sigma_adc #(.W ( 16 ))
+delta_sigma_adc_l (
+    .clk                     ( clk            ),
+    .rst_n                   ( rst_n          ),
+    .din                     ( rddat  [31:16] ),
+    .dout                    ( pdm_l          )
+);
+endmodule
+
+
